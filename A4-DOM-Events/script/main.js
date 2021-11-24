50d7cb827449bc282df5e290e48d117b8cdc279b
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2021-11-24 10:43:54 +0100

    a4 LocalStorage fast ready

--- A4-DOM-Events/script/main.js
+++ A4-DOM-Events/script/main.js
@@ -5,23 +5,37 @@ var main;
     var priceInput = (document.getElementById("price"));
     var dateInput = (document.getElementById("date"));
     var submit = (document.getElementById("submit"));
+    var load = (document.getElementById("load"));
+    var clear = (document.getElementById("load"));
+    var rows = [];
+    var loadRows = [];
+    var savedRows;
+    window.addEventListener("load", function () {
+        loadTable();
+    });
     submit.addEventListener("click", function () {
-        createEvent();
+        createEvent(artistInput.value, priceInput.value, dateInput.value.substring(0, 10), dateInput.value.substring(11, 16), true);
         setTimeout(function () {
             clearInput();
         }, 100);
     });
-    function createEvent() {
+    load.addEventListener("click", function () {
+        console.log(loadRows);
+        loadTable();
+    });
+    clear.addEventListener("click", function () {
+        localStorage.clear();
+    });
+    function createEvent(artistText, priceText, dateText, timeText, save) {
         var tableRow = document.createElement("tr");
         var artist = document.createElement("td");
-        artist.textContent = artistInput.value;
+        artist.textContent = artistText;
         var price = document.createElement("td");
-        price.textContent = priceInput.value;
+        price.textContent = priceText;
         var date = document.createElement("td");
-        var dateTime = dateInput.value;
-        date.textContent = dateTime.substring(0, 10);
+        date.textContent = dateText;
         var time = document.createElement("td");
-        time.textContent = dateTime.substring(11, 16);
+        time.textContent = timeText;
         var trashContainer = document.createElement("td");
         var trash = document.createElement("i");
         trash.className = "fa fa-trash";
@@ -32,6 +46,18 @@ var main;
         tableRow.appendChild(price);
         tableRow.appendChild(trashContainer);
         trashContainer.appendChild(trash);
+        if (save) {
+            var saveRow = {
+                artist: artist.textContent,
+                price: price.textContent,
+                date: date.textContent,
+                time: time.textContent
+            };
+            rows.push(saveRow);
+            savedRows = JSON.stringify(rows);
+            console.log(savedRows);
+            localStorage.setItem("savedRows", savedRows);
+        }
         trash.addEventListener("click", function () {
             table.removeChild(tableRow);
         });
@@ -40,6 +66,15 @@ var main;
         artistInput.value = "";
         priceInput.value = "";
         dateInput.value = "";
+    }
+    function loadTable() {
+        loadRows = JSON.parse(localStorage.getItem("savedRows"));
+        console.log(loadRows);
+        for (var i = 0; i < loadRows.length; i++) {
+            createEvent(loadRows[i].artist, loadRows[i].price, loadRows[i].date, loadRows[i].time, false);
+        }
+        rows = loadRows;
+        loadRows = [];
     }
 })(main || (main = {}));
 //# sourceMappingURL=main.js.map
--- A4-DOM-Events/script/main.ts
+++ A4-DOM-Events/script/main.ts
@@ -4,26 +4,49 @@ namespace main {
     const artistInput: HTMLInputElement = <HTMLInputElement>(document.getElementById("artist"));
     const priceInput: HTMLInputElement = <HTMLInputElement>(document.getElementById("price"));
     const dateInput: HTMLInputElement = <HTMLInputElement>(document.getElementById("date"));
-    const submit: HTMLElement = (document.getElementById("submit"));
+    const submit: HTMLButtonElement = <HTMLButtonElement>(document.getElementById("submit"));
+    const load: HTMLButtonElement = <HTMLButtonElement>(document.getElementById("load"));
+    const clear: HTMLButtonElement = <HTMLButtonElement>(document.getElementById("load"));
 
-    submit.addEventListener("click", (): void => {
-        createEvent();
-        setTimeout(function(): void {
-            clearInput();
-        }, 100);
+    interface TableRow {
+        artist: string;
+        price: string;
+        date: string;
+        time: string;
+    }
+
+    let rows: TableRow[] = [];    
+    let loadRows: TableRow[] = [];
+    let savedRows: string;
+
+    window.addEventListener("load", (): void => {
+        loadTable();
     });
 
-    function createEvent(): void {
+    submit.addEventListener("click", (): void => {
+        createEvent(artistInput.value, priceInput.value, dateInput.value.substring(0, 10), dateInput.value.substring(11, 16), true);
+        setTimeout(function(): void {
+            clearInput();
+        },         100);
+    });
+    load.addEventListener("click", (): void => {
+        console.log(loadRows);
+        loadTable();
+    });
+    clear.addEventListener("click", (): void => {
+        localStorage.clear();
+    });
+
+    function createEvent(artistText: string, priceText: string, dateText: string, timeText: string, save: boolean): void {
         let tableRow: HTMLElement = document.createElement("tr");
         let artist: HTMLElement = document.createElement("td");
-        artist.textContent = artistInput.value;
+        artist.textContent = artistText;
         let price: HTMLElement = document.createElement("td");
-        price.textContent = priceInput.value;
+        price.textContent = priceText;
         let date: HTMLElement = document.createElement("td");
-        let dateTime: string = dateInput.value;
-        date.textContent = dateTime.substring(0, 10);
+        date.textContent = dateText;
         let time: HTMLElement = document.createElement("td");
-        time.textContent = dateTime.substring(11, 16);
+        time.textContent = timeText;
         let trashContainer: HTMLElement = document.createElement("td");
         let trash: HTMLElement = document.createElement("i");
         trash.className = "fa fa-trash";
@@ -36,6 +59,20 @@ namespace main {
         tableRow.appendChild(trashContainer);
         trashContainer.appendChild(trash);
 
+        if (save) {
+            let saveRow: TableRow = {
+                artist: artist.textContent,
+                price: price.textContent,
+                date: date.textContent,
+                time: time.textContent
+            };
+            rows.push(saveRow);
+    
+            savedRows = JSON.stringify(rows);
+            console.log(savedRows);
+            localStorage.setItem("savedRows", savedRows);
+        }
+
         trash.addEventListener("click", (): void => {
             table.removeChild(tableRow);
         });
@@ -45,5 +82,16 @@ namespace main {
         artistInput.value = "";
         priceInput.value = "";
         dateInput.value = "";    
+    }
+
+    function loadTable(): void {
+        loadRows = JSON.parse(localStorage.getItem("savedRows"));
+        console.log(loadRows);
+        
+        for (let i: number = 0; i < loadRows.length; i++) {
+            createEvent(loadRows[i].artist, loadRows[i].price, loadRows[i].date, loadRows[i].time, false);
+        }
+        rows = loadRows;
+        loadRows = [];
     }
 }
